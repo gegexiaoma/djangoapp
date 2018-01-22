ff235adf98a846db7594bdf523b5aff7a1b1c5df
gegexiaoma <897226736@qq.com>
2018-01-22 22:36:34 +0800

    second

--- shopping/urls.py
+++ shopping/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'shopping.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'shop.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
